b5c9ab31247652e4bf177860f9a5ee62456c51d5
Chun Huang(Charles) <Charles546@users.noreply.github.com>
2022-04-29 10:53:30 -0700

    reusable config object

--- restapi/stock_price_grabber.go
+++ restapi/stock_price_grabber.go
@@ -19,32 +19,65 @@ import (
 	"github.com/Charles546/spicker/v2/restapi/operations"
 )
 
+// StockpricesGrabberConfig is a data structure storing the configs.
+type StockpricesGrabberConfig struct {
+	symbol string
+	ndays  int
+	url    string
+	err    middleware.Responder
+}
+
+// APIConfig is the loaded configs.
+var APIConfig *StockpricesGrabberConfig
+
+// APIPattern is a string pattern for creating the url.
 const APIPattern string = "https://www.alphavantage.co/query?apikey=%s&function=TIME_SERIES_DAILY&symbol=%s&outputsize=%s"
 
-func getStockprices(params operations.StockpricesParams) middleware.Responder {
-	symbol := os.Getenv("SYMBOL")
+func getConfig() *StockpricesGrabberConfig {
+	if APIConfig != nil {
+		return APIConfig
+	}
+
+	APIConfig = &StockpricesGrabberConfig{}
+	APIConfig.symbol = os.Getenv("SYMBOL")
 	ndaysStr := os.Getenv("NDAYS")
 	apikey := os.Getenv("ALPHAVANTAGE_APIKEY")
-	if len(symbol) == 0 {
-		return middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: SYMBOL")
+
+	if len(APIConfig.symbol) == 0 {
+		APIConfig.err = middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: SYMBOL")
+		return APIConfig
 	}
 	if len(ndaysStr) == 0 {
-		return middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: NDAYS")
+		APIConfig.err = middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: NDAYS")
+		return APIConfig
 	}
 	if len(apikey) == 0 {
-		return middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: ALPHAVANTAGE_APKEY")
+		APIConfig.err = middleware.Error(http.StatusInternalServerError, "configuration missing for operations.Stockprices: ALPHAVANTAGE_APKEY")
+		return APIConfig
 	}
-	ndays, err := strconv.Atoi(ndaysStr)
+
+	var err error
+	APIConfig.ndays, err = strconv.Atoi(ndaysStr)
 	if err != nil {
-		return middleware.Error(http.StatusInternalServerError, "invalid configuration for operations.Stockprices: NDAYS")
+		APIConfig.err = middleware.Error(http.StatusInternalServerError, "invalid configuration for operations.Stockprices: NDAYS")
+		return APIConfig
 	}
 	outputSize := "compact"
-	if ndays > 100 {
+	if APIConfig.ndays > 100 {
 		outputSize = "full"
 	}
 
-	url := fmt.Sprintf(APIPattern, apikey, symbol, outputSize)
-	resp, err := http.Get(url)
+	APIConfig.url = fmt.Sprintf(APIPattern, apikey, APIConfig.symbol, outputSize)
+	return APIConfig
+}
+
+func getStockprices(params operations.StockpricesParams) middleware.Responder {
+	c := getConfig()
+	if c.err != nil {
+		return c.err
+	}
+
+	resp, err := http.Get(c.url)
 
 	if err != nil {
 		log.Printf("operations.Stockprices api response error: %w\n", err)
@@ -66,12 +99,11 @@ func getStockprices(params operations.StockpricesParams) middleware.Responder {
 	sort.Slice(dates, func(i, j int) bool { return dates[i] > dates[j] })
 
 	ret := operations.StockpricesOKBody{}
-	ret.Symbol = &symbol
-	ret.History = make([]*models.Stockprice, ndays)
+	ret.Symbol = &c.symbol
+	ret.History = make([]*models.Stockprice, c.ndays)
 
 	var sum float32
-	count := 0
-	for i := range dates {
+	for i := 0; i < c.ndays; i++ {
 		prices := hist[dates[i]].(map[string]interface{})
 		open, _ := strconv.ParseFloat(prices["1. open"].(string), 32)
 		high, _ := strconv.ParseFloat(prices["2. high"].(string), 32)
@@ -86,16 +118,12 @@ func getStockprices(params operations.StockpricesParams) middleware.Responder {
 		day.Low = float32(low)
 		day.Close = float32(cl)
 
-		ret.History[count] = &day
+		ret.History[i] = &day
 
 		sum += day.Close
-		count++
-		if count == ndays {
-			average := sum / float32(ndays)
-			ret.Average = &average
-			break
-		}
 	}
+	average := sum / float32(c.ndays)
+	ret.Average = &average
 
 	return operations.NewStockpricesOK().WithPayload(&ret)
 }
